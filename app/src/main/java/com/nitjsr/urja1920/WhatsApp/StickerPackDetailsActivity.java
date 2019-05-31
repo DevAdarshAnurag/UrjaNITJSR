c4460d347ce602a85c36f03c875d16083df11994
Adarsh Anurag <a4adarshanurag@gmail.com>
2019-05-31 09:52:06 +0530

    sticker details activity ui change

--- app/src/main/java/com/nitjsr/urja1920/WhatsApp/StickerPackDetailsActivity.java
+++ app/src/main/java/com/nitjsr/urja1920/WhatsApp/StickerPackDetailsActivity.java
@@ -167,20 +167,6 @@ public class StickerPackDetailsActivity extends AddStickerPackActivity {
         }
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        //for hiding screen navigation buttons and show them when required by user; providing immersive display
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-        }
-    }
     private void updateAddUI(Boolean isWhitelisted) {
         if (isWhitelisted) {
             addButton.setVisibility(View.GONE);
