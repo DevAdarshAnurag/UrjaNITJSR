aba590c49003b92529b32e578b3cfd240e3fd52d
Adarsh Anurag <a4adarshanurag@gmail.com>
2019-10-10 21:03:09 +0530

    added 9 events map marker icons

--- app/src/main/java/com/nitjsr/urja1920/Activities/MapActivity.java
+++ app/src/main/java/com/nitjsr/urja1920/Activities/MapActivity.java
@@ -64,15 +64,49 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 .bearing(0)
                 .tilt(90)
                 .build();
-        int height = 100;
-        int width = 100;
-        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.lion);
+        int height = 64;
+        int width = 48;
+
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_badminton);
         Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
-        BitmapDescriptor smallMarkerIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+        BitmapDescriptor badmintonIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_basketball);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor basketballIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_football);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor footballIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_hockey);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor hockeyIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_athletics);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor athleticsIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_cricket);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor cricketIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_tabletennis);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor tabletennisIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_volleyball);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor volleyballIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+        b = BitmapFactory.decodeResource(getResources(), R.drawable.mp_chess);
+        smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        BitmapDescriptor chessIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        googleMap.addMarker(new MarkerOptions().position(hockeyGround).title("Hockey Ground").icon(smallMarkerIcon));
-        googleMap.addMarker(new MarkerOptions().position(tsg).title("TSG").icon(smallMarkerIcon));
-        googleMap.addMarker(new MarkerOptions().position(upsGround).title("Football Ground").icon(smallMarkerIcon));
+        googleMap.addMarker(new MarkerOptions().position(hockeyGround).title("Hockey").icon(hockeyIcon));
+        googleMap.addMarker(new MarkerOptions().position(tsg).title("Badminton").icon(badmintonIcon));
+        googleMap.addMarker(new MarkerOptions().position(upsGround).title("Football").icon(footballIcon));
 
         googleMap.setBuildingsEnabled(true);
         googleMap.getUiSettings().setAllGesturesEnabled(true);
