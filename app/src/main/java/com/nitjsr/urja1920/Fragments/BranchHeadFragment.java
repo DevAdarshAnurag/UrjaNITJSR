a37d2744073634f3eab6f66999d4471f2a2af11b
Adarsh Anurag <a4adarshanurag@gmail.com>
2019-10-17 18:53:25 +0530

    Added more photos and added border in notifications activity

--- app/src/main/java/com/nitjsr/urja1920/Fragments/BranchHeadFragment.java
+++ app/src/main/java/com/nitjsr/urja1920/Fragments/BranchHeadFragment.java
@@ -66,11 +66,11 @@ public class BranchHeadFragment extends Fragment {
 
         ArrayList<TeamPerson> cse=new ArrayList<TeamPerson>();
         cse.add(new TeamPerson("Sravya",R.drawable.lavu_sravya,"9182884051","https://www.instagram.com/sravyalavu"));
-        cse.add(new TeamPerson("Aman Srivastava",R.drawable.avatar,"7903764490","https://www.instagram.com/"));
+        cse.add(new TeamPerson("Aman Srivastava",R.drawable.aman_srivastava,"7903764490","https://www.instagram.com/"));
 
         ArrayList<TeamPerson> ece=new ArrayList<TeamPerson>();
-        ece.add(new TeamPerson("Amit Prakash",R.drawable.amit_prakash,"7992324380","https://www.instagram.com/im_amit_p"));
         ece.add(new TeamPerson("Sweta Kumari",R.drawable.sweta_kumari,"8210938009","https://www.instagram.com/sk_raj52"));
+        ece.add(new TeamPerson("Amit Prakash",R.drawable.amit_prakash,"7992324380","https://www.instagram.com/im_amit_p"));
 
         ArrayList<TeamPerson> eee=new ArrayList<>();
         eee.add(new TeamPerson("Shalini",R.drawable.shalini_sinha,"7870328685","http://instagram.com/_shalinisinha_"));
@@ -79,21 +79,26 @@ public class BranchHeadFragment extends Fragment {
 
         ArrayList<TeamPerson> me=new ArrayList<>();
         me.add(new TeamPerson("Amulya Toppo",R.drawable.amulya_toppo,"9771229237","https://www.instagram.com/7_dorcas"));
+        me.add(new TeamPerson("Abhishek Anand",R.drawable.abhishek_anand,"9931183228","https://www.instagram.com/abhishek_6127"));
+
 
         ArrayList<TeamPerson> mme=new ArrayList<>();
         mme.add(new TeamPerson("Mansi",R.drawable.mansi_singh,"9973356195","https://www.instagram.com/m.s._.drapetomaniac "));
         mme.add(new TeamPerson("Aditya Kumar",R.drawable.aditya_kumar,"9939721444","https://www.instagram.com/kr_aditya28"));
 
         ArrayList<TeamPerson> pie=new ArrayList<>();
-        pie.add(new TeamPerson("Abhishek Kumar",R.drawable.abhishek_kumar,"7004129994","https://www.instagram.com/kumarabhi72/"));
         pie.add(new TeamPerson("Jyoti Nirala",R.drawable.jyoti_nirala,"7587309970","https://www.instagram.com/nirala_j/"));
+        pie.add(new TeamPerson("Abhishek Kumar",R.drawable.abhishek_kumar,"7004129994","https://www.instagram.com/kumarabhi72/"));
 
         ArrayList<TeamPerson> ce=new ArrayList<>();
         ce.add(new TeamPerson("Simran Kour",R.drawable.simran_kour,"9835330142","https://www.instagram.com/simran_kour735/"));
+        ce.add(new TeamPerson("Rohit Kumar",R.drawable.rohit_kumar,"7992238518","https://www.instagram.com/rohit_ps16/"));
 
 
         ArrayList<TeamPerson> mcamsc=new ArrayList<>();
+        mcamsc.add(new TeamPerson("Nitu Nivedita",R.drawable.nitu_nivedita_chore,"7485074418","https://www.instagram.com/nitu.nivedita"));
         mcamsc.add(new TeamPerson("Jitendranath Tudu",R.drawable.jitendranath_tudu,"9046332109","https://www.instagram.com/tudu_j"));
+
 
         persons.add(cse);
         persons.add(ece);
