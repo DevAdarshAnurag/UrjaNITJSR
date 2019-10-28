bef939edb4bff5d841d743127878cbdfe7a5ae14
Adarsh Anurag <a4adarshanurag@gmail.com>
2019-10-28 16:35:13 +0530

    animation added to events fragment

--- app/src/main/java/com/nitjsr/urja1920/Adapters/EventAdapter.java
+++ app/src/main/java/com/nitjsr/urja1920/Adapters/EventAdapter.java
@@ -1,10 +1,14 @@
 package com.nitjsr.urja1920.Adapters;
 
 import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,7 +22,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
     private LayoutInflater inflater;
     private Context context;
     private List<Events> list;
-
+    private int lastPosition =-1;
     private onItemClicked onClick;
 
     public EventAdapter(Context context, List<Events> list) {
@@ -27,23 +31,36 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
         inflater = LayoutInflater.from(context);
     }
 
+    @NonNull
     @Override
-    public EventViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = inflater.inflate(R.layout.event_item_layout, parent, false);
         return (new EventViewHolder(itemView));
     }
 
     @Override
-    public void onBindViewHolder(EventViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
         Events curr = list.get(position);
         //holder.event_name.setText(curr.getEventName());
         holder.event_image.setImageResource(curr.getEventThumbnail());
+        setAnimation(holder.itemView, position);
         holder.event_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onClick.onItemClick(position);
             }
         });
+    }
+
+    private void setAnimation(View view, int position) {
+        if (lastPosition < position) {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_from_bottom);
+            view.startAnimation(animation);
+        } else {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_from_top);
+            view.startAnimation(animation);
+        }
+        lastPosition = position;
     }
 
     @Override
@@ -61,13 +78,10 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
 
     public class EventViewHolder extends RecyclerView.ViewHolder {
         public ImageView event_image;
-        //public TextView event_name;
 
         public EventViewHolder(View itemView) {
             super(itemView);
             event_image = itemView.findViewById(R.id.event_image);
-            //event_name = itemView.findViewById(R.id.event_name);
-
         }
     }
 }
